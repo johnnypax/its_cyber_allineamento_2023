a2f5fd71ebf95f36bfd8156709b1d9090902eb60
Giovanni Pace <braintechinnovations@gmail.com>
2023-11-09 16:05:54 +0100

    Primo commit

---
+++ CorsoITSCybersecOop/src/its/lez00/HelloWorld.java
@@ -0,0 +1,12 @@
+package its.lez00;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		System.out.println("Ciao Giovanni");
+		
+	}
+
+}
